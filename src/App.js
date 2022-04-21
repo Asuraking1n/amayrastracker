6b3d586e6c60d97aaa7166d10796221ba9003a90
Asuraking1n <nishant99tiwari@gmail.com>
2022-04-21 05:52:13 -0700

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Homepage from "./pages/HomePage/Homepage";
-
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from "./pages/LoginSignup/Login";
 
 function App() {
   return (
     <>
-    <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/LoginSignup/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './login.css'
+const Login = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Login
